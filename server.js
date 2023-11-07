cbf492120764d2743157ab8caab394604b3cc377
sabbathandco <141058023+sabbathandco@users.noreply.github.com>
2023-11-06 21:54:48 -0500

    Trying out Dall-E (and failing)

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const openai = new OpenAIApi(configuration);
 app.use(express.json());
 app.use(express.static('public'));
 
+// Endpoint to create dialogue using the OpenAI GPT model
 app.post('/create-dialogue', async (req, res) => {
   const { panel, content, beginningDialogue, middleDialogue } = req.body;
   const prompt = `Create dialogue for the ${panel} of a comic strip: ${content}`;
@@ -24,7 +25,6 @@ app.post('/create-dialogue', async (req, res) => {
     previousDialogues.push({ role: "system", content: beginningDialogue });
     previousDialogues.push({ role: "system", content: middleDialogue });
   }
-
   try {
     const response = await openai.createChatCompletion({
       model: "gpt-4",
@@ -34,7 +34,6 @@ app.post('/create-dialogue', async (req, res) => {
       }]),
       max_tokens: 150,
     });
-
     res.json({ dialogue: response.data.choices[0].message.content });
   } catch (error) {
     console.error('Error calling OpenAI:', error);
@@ -42,24 +41,36 @@ app.post('/create-dialogue', async (req, res) => {
   }
 });
 
+// Endpoint to generate images using DALL-E 3
 app.post('/generate-image', async (req, res) => {
   const { dialogue } = req.body;
-  const prompt = `Generate an image for a comic strip with dialogue: ${dialogue}`;
+
+  // Construct the prompt for image generation
+  const prompt = `An illustration for the following dialogue: "${dialogue}"`;
 
   try {
-    // Call to DALL-E API (Placeholder comment as the actual API syntax may vary)
-    // const response = await openai.createImage({ ... });
-
-    // Simulate obtaining the image URL from the response
-    const imageUrl = 'URL_FROM_DALL-E_RESPONSE';
-
-    res.json({ imageUrl });
+    // Call the OpenAI DALL-E 3 API for image generation
+    const imageResponse = await openai.createImage({
+      model: "dall-e-3", // Use DALL-E 3 for image generation
+      prompt: prompt,
+      n: 1,
+      size: "1024x1024", // Requested image resolution
+      quality: "standard" // Can be "standard" or "hd" for high detail
+    });
+    // Extract the URL of the generated image
+    const imageUrl = imageResponse.data[0].url;
+    res.json({ imageUrl: imageUrl });
   } catch (error) {
-    console.error('Error calling OpenAI for image generation:', error);
-    res.status(500).send('Error generating image.');
+    console.error('Failed to generate DALL-E image:', error);
+    // Log the full error response if it's an API error for more details
+    if (error.response) {
+      console.error('API response error:', error.response.data);
+    }
+    res.status(500).send('Error in image generation');
   }
 });
 
+// Start the server
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
