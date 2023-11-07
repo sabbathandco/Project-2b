93df3e8a5b099cb4fafbda3d28e5a018cd95b62d
sabbathandco <141058023+sabbathandco@users.noreply.github.com>
2023-11-06 21:26:39 -0500

    Dora Blessings

--- public/script.js
+++ public/script.js
@@ -6,9 +6,7 @@ async function generateAndDisplayImage(panelId, dialogue) {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-                dialogue: dialogue
-            }),
+            body: JSON.stringify({ dialogue }),
         });
 
         if (!response.ok) {
@@ -17,15 +15,15 @@ async function generateAndDisplayImage(panelId, dialogue) {
 
         const data = await response.json();
         const imageElement = document.getElementById(`${panelId}-image`);
-        // Suppose the response contains a URL to the image
-        imageElement.innerHTML = `<img src="${data.imageUrl}" alt="Generated Image for ${panelId}"/>`;
+        // Displaying the image on the webpage
+        imageElement.innerHTML = `<img src="${data.imageUrl}" alt="Generated Image for ${panelId}" />`;
     } catch (error) {
         console.error('There has been a problem with your fetch operation:', error);
     }
 }
+
 // Function to generate dialogue for a comic panel
 async function generateDialogue(panelId) {
-    // Obtain the user input for each panel
     let userInput;
     if (panelId === 'beginning') {
         userInput = document.getElementById('userInput').value;
@@ -37,7 +35,6 @@ async function generateDialogue(panelId) {
 
     const panel = document.getElementById(panelId);
 
-    // Collect previous dialogues if necessary
     let previousDialogues = {};
     if (panelId === 'middle') {
         previousDialogues.beginningDialogue = document.getElementById('userInput').value;
@@ -45,7 +42,7 @@ async function generateDialogue(panelId) {
         previousDialogues.beginningDialogue = document.getElementById('userInput').value;
         previousDialogues.middleDialogue = document.getElementById('middleInput').value;
     }
-  
+
     try {
         const response = await fetch('/create-dialogue', {
             method: 'POST',
@@ -55,17 +52,15 @@ async function generateDialogue(panelId) {
             body: JSON.stringify({
                 panel: panelId,
                 content: userInput,
-                ...previousDialogues,
+                ...previousDialogues
             }),
         });
-    
+
         if (!response.ok) {
             throw new Error('Network response was not ok');
         }
-    
+        
         const data = await response.json();
-
-        // Update the panel's content correctly
         const dialogueElement = panel.querySelector('.dialogue');
         if (dialogueElement) {
             dialogueElement.textContent = data.dialogue;
@@ -75,7 +70,10 @@ async function generateDialogue(panelId) {
             newDialogue.textContent = data.dialogue;
             panel.appendChild(newDialogue);
         }
+
+        // When the dialogue has been generated, generate the corresponding image
+        generateAndDisplayImage(panelId, data.dialogue);
     } catch (error) {
-        console.error('There has been a problem with your fetch operation:', error);
+        console.error('There was a problem with your fetch operation:', error);
     }
 }
--- server.js
+++ server.js
@@ -1,101 +1,65 @@
 const express = require('express');
 const { Configuration, OpenAIApi } = require('openai');
 
-// Load environment variables from .env file
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Initialize OpenAI API configuration
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
 const openai = new OpenAIApi(configuration);
 
-// Middleware to parse JSON and serve static files
 app.use(express.json());
 app.use(express.static('public'));
 
-// Route to handle dialogue creation requests
 app.post('/create-dialogue', async (req, res) => {
-  // Extract the panel and content from the request body
   const { panel, content, beginningDialogue, middleDialogue } = req.body;
-
-  // Initialize the prompt based on the panel provided
-  const prompt = `Create a dialog among two characters for the ${panel} of a three-frame comic book. The user of the comic book creation website will be responding to this prompt.\n\n${content}`;
-  
-  // Initialize the previousDialogues array based on the panel
+  const prompt = `Create dialogue for the ${panel} of a comic strip: ${content}`;
   let previousDialogues = [];
   if (panel === 'middle') {
-    previousDialogues.push({
-      role: "assistant",
-      content: beginningDialogue
-    });
+    previousDialogues.push({ role: "system", content: beginningDialogue });
   } else if (panel === 'end') {
-    previousDialogues.push({
-      role: "assistant",
-      content: beginningDialogue
-    }, {
-      role: "assistant",
-      content: middleDialogue
-    });
+    previousDialogues.push({ role: "system", content: beginningDialogue });
+    previousDialogues.push({ role: "system", content: middleDialogue });
   }
-  // Route to handle image generation requests
-app.post('/generate-image', async (req, res) => {
-  const { dialogue } = req.body;
-
-  // Construct the prompt for image generation
-  const prompt = `An illustration for the following dialogue: "${dialogue}"`;
 
   try {
-    // Call the OpenAI API for image generation
-    // (adjust parameters as needed for your specific use case)
-    const response = await openai.createImage({
-      prompt: prompt,
-      n: 1, // Number of images to generate
-      size: "1024x1024" // Image resolution
-    });
-
-    // Suppose the response contains data with the image URL
-    // Send the generated image URL back in the response
-    res.json({ imageUrl: response.data.data[0].url });
-  } catch (error) {
-    console.error('Error calling the OpenAI API:', error);
-    res.status(500).send('An error occurred while generating the image.');
-  }
-});
-  
-  try {
-    // Call the OpenAI API with the constructed messages array
     const response = await openai.createChatCompletion({
       model: "gpt-4",
       messages: previousDialogues.concat([{
-        role: "system",
-        content: `You are a comic book writer. You write three-frame comic books that are entertaining, make people want to check out your next comic, and make people laugh.`
-      }, {
         role: "user",
-        content: "I am a user of your comic book creation website."
-      }, {
-        role: "assistant",
         content: prompt
       }]),
-      temperature: 0.7, // Adjusted for creativity but not too random
-      max_tokens: 150, // Adjusted based on expected length of dialogue
-      top_p: 1,
-      frequency_penalty: 0,
-      presence_penalty: 0,
+      max_tokens: 150,
     });
 
-    // Send the generated dialogue back in the response
     res.json({ dialogue: response.data.choices[0].message.content });
   } catch (error) {
-    console.error('Error calling the OpenAI API:', error);
-    res.status(500).send('An error occurred while generating the dialogue.');
+    console.error('Error calling OpenAI:', error);
+    res.status(500).send('Error generating dialogue.');
   }
 });
 
-// Start the server
+app.post('/generate-image', async (req, res) => {
+  const { dialogue } = req.body;
+  const prompt = `Generate an image for a comic strip with dialogue: ${dialogue}`;
+
+  try {
+    // Call to DALL-E API (Placeholder comment as the actual API syntax may vary)
+    // const response = await openai.createImage({ ... });
+
+    // Simulate obtaining the image URL from the response
+    const imageUrl = 'URL_FROM_DALL-E_RESPONSE';
+
+    res.json({ imageUrl });
+  } catch (error) {
+    console.error('Error calling OpenAI for image generation:', error);
+    res.status(500).send('Error generating image.');
+  }
+});
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
